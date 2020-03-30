10f0e31ca8f096a339a1c66457707e8161dde185
vaibhav-s-i-n-g-h <38386739+vaibhav-s-i-n-g-h@users.noreply.github.com>
2020-03-30 16:03:26 +0530

    Update face.py

--- face_recognition/face.py
+++ face_recognition/face.py
@@ -1,1 +1,49 @@
+import cv2
+import PIL
+import numpy as np
+import os
+import pickle
 
+face_cascade = cv2.CascadeClassifier('C:/Users/vs241/Desktop/face_rec/haarcascades/haarcascade_frontalface_alt2.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person_name":1}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+    
+cap = cv2.VideoCapture(0)
+
+while(True):
+
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors = 5)
+
+    for (x,y,w,h) in faces:
+        
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        id_, conf = recognizer.predict(roi_gray)
+
+        if conf >= 65:    
+            font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+            name = labels[id_]
+            color = (0, 255, 0)
+            stroke = 1
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        color = (0,0,255)
+        stroke = 3
+        cv2.rectangle(frame, (x,y), (x+w,y+h), color, stroke)
+
+    cv2.imshow('frmae', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
